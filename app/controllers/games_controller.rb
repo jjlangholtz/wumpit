aaeff47fe128a172f114574588b9b75b153f67f9
Joshua Langholtz <jjlangholtz@gmail.com>
2014-06-26 17:16:31 -0400

    Player can now move around cave

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,17 @@ class GamesController < ApplicationController
   def move
     @game = Game.first
     @room = @game.room
+    @game.update(player: params[:player])
     @player = @game.player
     @next_rooms = @game.rooms_available(@player)
     @first_room = @next_rooms[0]
     @second_room = @next_rooms[1]
     @third_room = @next_rooms[2]
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:player)
   end
 end
